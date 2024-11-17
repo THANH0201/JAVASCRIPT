0210a84afd8528105c3e362b87e6a49c983fde26
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2024-11-17 16:46:41 +0200

    MODULE 2

--- MODULE 2/TASK 10/Q10.js
+++ MODULE 2/TASK 10/Q10.js
@@ -1,5 +1,20 @@
 'use strict';
 const target = document.querySelector('#target');
+/*
+function countOccurrences(arr) {
+    let counts = {};
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    return counts;
+}
+
+// Ví dụ sử dụng
+let array = [1, 2, 3, 1, 2, 1];
+let result = countOccurrences(array);
+console.log(result); // { '1': 3, '2': 2, '3': 1 }
+*/
 //candidates:
 
 const candidate = ~~prompt('How many candidates are there?')
@@ -8,40 +23,35 @@ let candidates = [];
 function candidateList(name,vote = 0) {
     for (let i = 0; i < candidate; i++) {
         const list = Object.create({})
-        const names = prompt(`Name of candidate ${i+1}:`);
+        const names = prompt(`Name of candidate ${i + 1}:`);
         list.name = names;
         list.vote = vote;
         candidates.push(list)
-//        const result = `<li>${list.name}</li>`
-//       target.innerHTML += result;
     }
     console.log('candidateList', candidates)
-
 }
 candidateList(name,0);
 
-//voters:
-const voter = ~~prompt('How many voters are there?')
 let list_vote = []
-function voting(vote) {
+function voting(list_vote) {
+    const voter = ~~prompt('How many voters are there?')
+
     for (let i = 0; i < voter; i++) {
         const vote = prompt(`Who do you want to vote?`);
         list_vote.push(vote);
+//        list_vote.sort(a,b => a.vote - b.vote);
     }
-    let count = 0
-    for (let j = 0; j < list_vote.length; j++) {
-        if (list_vote[j] === name) {
-            count ++;
-        }
+    console.log('voterList', list_vote)
 
-
-    }
-
-    const result = `<li>${list_vote}</li>`
-    target.innerHTML += result;
+    //console.log(`The winner is ${} with ${} votes.\n results: ${list_vote}`);
 }
+voting(list_vote);
 
-voting(vote);
+
+
+
+
+
 
 
 
